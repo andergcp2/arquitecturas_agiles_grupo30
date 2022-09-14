b47f917b1e0bb68479a6772763555c4afc73f7df
magon-miso <m.agonf@uniandes.edu.co>
2022-09-13 23:08:24 -0500

    pruebas redis docker win11

--- Experimento Arquitectura 1/monitor/main.py
+++ Experimento Arquitectura 1/monitor/main.py
@@ -7,49 +7,47 @@ import database
 from modelos import MonitorLog
 import redis
 
-redis_cli = redis.Redis(host="localhost", password="redispw", port=49153, decode_responses=True, encoding="utf-8", )
+print("monitor init ..")
+#redis_cli = redis.Redis(host="localhost", password="redispw", port=49153, decode_responses=True, encoding="utf-8", )
+redis_cli = redis.Redis(host="localhost", password="", port=6379, decode_responses=True, encoding="utf-8", )
 pubsub = redis_cli.pubsub()
-tiempo_maximo_respuesta = 3
+pubsub.subscribe('eco')
+print("monitor init ok")
+eco_max_time = 2
+
+def send_ping_job():
+    ping = MonitorLog(False, "SensorVigilant1", datetime.now())
+    database.session.add(ping)
+    database.session.commit()
+    redis_cli.publish('ping', ping.id)
+    #print(datetime.now(), ping.id)
 
 def run():
     while True:
         schedule.run_pending()
-        respuesta_eco = pubsub.get_message(ignore_subscribe_messages=True)
-        if respuesta_eco is not None:
-            respuesta_mensaje = json.loads(respuesta_eco['data'])
-            log = MonitorLog(respuesta=True, servicio_monitoreado=respuesta_mensaje['service'], time_log=None)
-            log.id = respuesta_mensaje['id']
+        resp = pubsub.get_message(ignore_subscribe_messages=True)
+        if resp is not None:
+            eco = json.loads(resp['data'])
+            log = MonitorLog(respuesta=True, servicio_monitoreado=eco['service'], time_log=None)
+            log.id = eco['id']
             database.session.merge(log)
             database.session.commit()
-            
-def job():
-    mensaje_eco = MonitorLog(False, "SensorVigilant1", datetime.now())
-    database.session.add(mensaje_eco)
-    database.session.commit()
-    redis_cli.publish('ping', mensaje_eco.id)
-    
-def verificar_fallas():
-    time_menos = datetime.now() - timedelta(seconds=tiempo_maximo_respuesta)
-    mensajes_log = database.session.query(MonitorLog).filter(MonitorLog.reportada == False)\
+
+def check_fails_job():
+    time_menos = datetime.now() - timedelta(seconds=eco_max_time)
+    pings = database.session.query(MonitorLog).filter(MonitorLog.reportada == False)\
                                             .filter(MonitorLog.respuesta == False)\
                                             .filter(MonitorLog.time_log < time_menos).all()
-    if mensajes_log is not None:        
-        for registro in mensajes_log:
-            with open('log_fallas_reportadas.txt','a+') as file:
-                file.write('{} - Falla reportada id: {}\n'.format(datetime.now(), registro.id))
-            registro.reportada = True
-            database.session.add(registro)
+    if pings is not None:        
+        for ping in pings:
+            with open('fails_reported.log', 'a+') as file:
+                file.write('{} - Falla reportada id: {}\n'.format(datetime.now(), ping.id))
+            ping.reportada = True
+            database.session.add(ping)
             database.session.commit()
 
 if __name__ == '__main__':
     database.Base.metadata.create_all(database.engine)
-    pubsub.subscribe('eco')
-    schedule.every().second.do(job)
-    schedule.every(tiempo_maximo_respuesta).seconds.do(verificar_fallas)
+    schedule.every().second.do(send_ping_job)
+    schedule.every(eco_max_time).seconds.do(check_fails_job)
     run()
-
-
-
-
-
-
--- Experimento Arquitectura 1/sensorVigilant/sensorVigilant.py
+++ Experimento Arquitectura 1/sensorVigilant/sensorVigilant.py
@@ -4,22 +4,24 @@ from random import randint
 import time
 import redis
 
-redis_cli = redis.Redis(host="localhost", password="redispw", port=49153, decode_responses=True, encoding="utf-8", )
-
+print("sensor vigilant init ..")
+#redis_cli = redis.Redis(host="localhost", password="redispw", port=49153, decode_responses=True, encoding="utf-8", )
+redis_cli = redis.Redis(host="localhost", password="", port=6379, decode_responses=True, encoding="utf-8", )
 pubsub = redis_cli.pubsub()
 pubsub.subscribe('ping')
+print("sensor vigilant init ok")
 
 while True:
-    res = pubsub.get_message(ignore_subscribe_messages=True)
-    time.sleep(1)
-    random_number = randint(1, 20)
-    if res is None:
+    ping = pubsub.get_message(ignore_subscribe_messages=True)
+    #time.sleep(1)
+    random_number = randint(1, 3)
+    if ping is None:
         continue
     if random_number != 1:
-        respuesta = {"id": res['data'], "service" : "sensorVigilant1"}
-        redis_cli.publish('eco', json.dumps(respuesta))
-        res = None
-    elif random_number == 1:
-        id_reportado = res['data']
-        with open('log_fallas.txt','a+') as file:
-            file.write('{} - Falla generada id: {}\n'.format(datetime.now(), id_reportado))
+        eco = {"id": ping['data'], "service" : "SV-1"}
+        redis_cli.publish('eco', json.dumps(eco))
+        #print(datetime.now(), eco)
+        ping = None
+    elif random_number == 1:        
+        with open('fails_generated.log', 'a+') as file:
+            file.write('{} - Falla generada id: {}\n'.format(datetime.now(), ping['data']))
