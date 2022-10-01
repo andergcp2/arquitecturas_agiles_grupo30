342b5fd2e592feb53beea79f27b9c865e7209680
andergcp2 <ag.castiblanco1207@uniandes.edu.co>
2022-10-01 12:13:16 -0500

    Add: Implmento envío de mensaje a través de Redis

--- seguridad/validator/vistas.py
+++ seguridad/validator/vistas.py
@@ -4,12 +4,20 @@ from flask_jwt_extended import create_access_token
 from flask_restful import Resource, Api
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import or_, desc
+import json
+import redis
+
 #import requests
 
 #from modelos import db, Policy, PolicySchema
 #policy_schema = PolicySchema()
 
 class VistaSecurityCheck(Resource):
+
+    def __init__(self) -> None:
+        # self.redis_cli = redis.Redis(host="localhost", password="redispw", port=6379, decode_responses=True, encoding="utf-8", )
+        self.redis_cli = redis.Redis(host="localhost", port=6379, decode_responses=True, encoding="utf-8", )
+        super().__init__()
 
     def post(self):
         authorized = self.is_authorized(request.headers)
@@ -38,6 +46,7 @@ class VistaSecurityCheck(Resource):
         ip = headers["Test-IP"]
         ips = ['10.20.0.1', '10.20.0.2', '10.20.0.3']
         if ip not in ips:
+            self.send_sec_alert("sec", {"status": "400", "mensaje": "security check black list IP: "+ ip, "receptores": "['admin']"})
             return {"status": "400", "mensaje": "security check black list IP: "+ ip}
         return True
 
@@ -45,6 +54,7 @@ class VistaSecurityCheck(Resource):
         city = headers["Test-City"]
         cities = ['bogota', 'cali', 'killa']
         if city not in cities:
+            self.send_sec_alert("code-sec", {"status": "403", "mensaje": "security check wrong location: "+ city, "receptores": "['admin']"})
             return {"status": "403", "mensaje": "security check wrong location: "+ city}
         return True
 
@@ -53,7 +63,13 @@ class VistaSecurityCheck(Resource):
         if time:
             dt = datetime.strptime(time, '%d.%m.%Y %H:%M:%S')
             if dt.hour >= 0 and dt.hour < 5:
+                self.send_sec_alert("sec", {"status": "403", "mensaje": "security check suspicious time: "+ time, "receptores": "['admin']"})
                 return {"status": "403", "mensaje": "security check suspicious time: "+ time}
         return True
 
-    # def send_sec_alert(self, topico, mensaje)
+    def send_sec_alert(self, topico, mensaje):
+        respuesta = self.redis_cli.publish(topico, json.dumps(mensaje))
+        # print("Mensaje para cola")
+        # print(json.dumps(mensaje))
+        # print("respuesta redis:")
+        # print(respuesta)
